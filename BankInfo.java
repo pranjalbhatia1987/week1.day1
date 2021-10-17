bd45ec28736b85cbbbb9d6992d107f8406bbdad6
pranjalbhatia1987 <91780698+pranjalbhatia1987@users.noreply.github.com>
2021-10-17 23:58:14 +0100

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,23 @@
+package org.Bank;
+
+public class BankInfo {
+	
+	public void saving()
+	{
+		System.out.println("This is savings acc");
+		
+	}
+	
+	public void fixed()
+	{
+		System.out.println("this is fixed acc");
+		
+	}
+	
+	public void deposit()
+	{
+		System.out.println("This is deposit acc");
+		
+	}
+
+}
